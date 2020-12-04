edce42956de5cbd44c2700395de2e6b1e4b91f56
Kimia Ghasemi <kimiagh14@gmail.com>
2020-12-04 21:42:39 +0330

    First part

---
+++ weather/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const api = {
+  key:"c837066d90d230414d6c43c0774187c0",
+  base: "https://api.openweathermap.org/data/2.5"
+}
+function App() {
+  return (
+    <div className="App">
+    <main>
+
+    </main>
+    </div>
+  );
+}
+
+export default App;
